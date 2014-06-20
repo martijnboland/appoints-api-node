aa4a3c750b81457a1075d7c0c6506594010a4bb6
Martijn Boland <martijn@boland.org>
2014-06-20 10:02:48 +0200

    Enabled CORS

--- config/example.js
+++ config/example.js
@@ -16,7 +16,11 @@ var settings = {
       callbackUrl: 'http://localhost:3000/auth/google/callback' 
     }
   },
-  tokenSecret: 'my super duper shared secret'
+  tokenSecret: 'my super duper shared secret',
+  cors: {
+    origin: "http://localhost:3001",
+    methods: [ "GET", "POST", "PUT", "PATCH", "DELETE", "HEAD" ]
+  }
 };
 
 exports.settings = settings;
--- config/test.js
+++ config/test.js
@@ -21,7 +21,11 @@ var settings = {
       callbackUrl: 'http://localhost:3000/auth/google/callback' 
     }
   },
-  tokenSecret: 'test token secret'
+  tokenSecret: 'test token secret',
+  cors: {
+    origin: "http://localhost:3001",
+    methods: [ "GET", "POST", "PUT", "PATCH", "DELETE", "HEAD" ]
+  }
 };
 
 exports.settings = settings;
