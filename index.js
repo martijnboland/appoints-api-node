2e5a256d78aa1e6966495b94f52711352bd42ef2
Martijn Boland <martijn@boland.org>
2016-06-22 22:55:52 +0200

    Use old Mongoose promise impl.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var config = require('./config');
 var mongoose = require('mongoose');
 var server = require('./server');
 
-mongoose.Promise = global.Promise;
 mongoose.connect(config.settings.db.connectionString);
 
 server.start();
