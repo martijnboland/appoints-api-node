9f88f36411489ab6849589a48ef02affdb28d18e
Martijn Boland <martijn@boland.org>
2019-04-04 10:47:17 +0200

    Updated mongoose

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var server = require('./server');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.settings.db.connectionString);
+mongoose.connect(config.settings.db.connectionString, {useNewUrlParser: true, useCreateIndex: true });
 
 server.start();
